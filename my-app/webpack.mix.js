d740d80b29abd0ced64369777f8b894580f681c3
caterina <catemarian1806@gmail.com>
2020-08-28 15:36:34 +0200

    creazione strurrura

---
+++ my-app/webpack.mix.js
@@ -0,0 +1,9 @@
+let mix = require('laravel-mix');
+
+
+mix
+  .js('src/app.js', 'dist/')
+  .sass('src/app.scss', 'dist/')
+  .copy('src/*.html', 'dist/')
+  .copyDirectory('src/imgs', 'dist/imgs')
+;
